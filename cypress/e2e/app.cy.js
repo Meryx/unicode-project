b11d7db2090f4811b72e4eac2d37b04b6e20c269
Meryx <anwar.haredy@gmail.com>
2024-10-10 11:50:31 +0300

    .

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,7 +1,7 @@
 describe("Navigation", () => {
   it("should navigate to the about page", () => {
-    // Start from the index page
-    cy.visit("http://localhost:3000/");
+    // Start from the index page, using the baseUrl defined in the config
+    cy.visit("/");
 
     // Find a link with an href attribute containing "about" and click it
     cy.get('a[href*="about"]').click();
