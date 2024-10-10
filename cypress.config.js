8d775c27dc91070f8e3597acc50648baa85e9c97
Meryx <anwar.haredy@gmail.com>
2024-10-10 11:34:33 +0300

    .

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL || "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
